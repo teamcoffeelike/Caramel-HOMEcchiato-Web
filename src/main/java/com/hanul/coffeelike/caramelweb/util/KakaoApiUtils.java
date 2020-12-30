ed2c2801d7b07169c9e497103a61fd64b042619d
iwasamistake <iwasamistake4@gmail.com>
2020-12-30 14:24:02 +0900

    카카오 API 키
    Signed-off-by: iwasamistake <iwasamistake4@gmail.com>

---
+++ src/main/java/com/hanul/coffeelike/caramelweb/util/KakaoApiUtils.java
@@ -0,0 +1,7 @@
+package com.hanul.coffeelike.caramelweb.util;
+
+public final class KakaoApiUtils{
+	private KakaoApiUtils(){}
+
+	public static final String KAKAO_API_KEY = "eb19ab2e4255e8804a47e5f8a03faede";
+}
